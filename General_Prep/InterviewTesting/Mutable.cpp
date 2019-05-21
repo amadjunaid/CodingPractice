14eafd5514d21241fb16d0dcb819631cb472ae3f
Amad Junaid <amad.64bit@gmail.com>
2019-05-21 16:48:56 +0200

    Coding Practice

--- General_Prep/InterviewTesting/Mutable.cpp
+++ General_Prep/InterviewTesting/Mutable.cpp
@@ -1,71 +1,48 @@
+// weakPtr.cpp
+
 #include <iostream>
 #include <memory>
+#include <map>
 #include <vector>
-#include <string>
+#include <cctype>
 
-using std::cout;
-using std::string;
-class Mammal
+std::vector<char> specialchars = { ',', '.','!', '\'', ' ' };
+bool isSpecialChar(char r_c)
 {
-protected:
-	 string name_;
-	 mutable int size;
-public:
-	Mammal()
-	{
-		name_ = "nameless";
-	}
-	Mammal(const string& name)
-	{
-		name_ = name;
-	}
+	for (auto c : specialchars)
+		if (r_c == c) return true;
 
-	void eat()
-	{
-		cout << "This Mammal " << name_ << " is eating" << std::endl;
-	}
-
-	void foo() const
-	{
-		size = 10;
-	}
-};
-
-class Tiger : public virtual Mammal
-{
-public:
-	void groom()
-	{
-		cout << "This Mammal " << name_ << " is T_groomed" << std::endl;
-	}
-};
-
-
-void F(int A[], int B[], int n)
-{
-	for (int i = 0; i<n;i++)
-	{
-		auto res = [](int A[],int n, int i)->int {
-			int result = 1;
-			for (int j = 0; j < n; j++)
-			{
-				if (i != j)
-					result *= A[j];
-			}
-			return result;
-		};
-
-		B[i] = res(A,n,i);
-		
-	}
-
+	return false;
 }
 
-int main()
-{
-	int A[] = { 2, 1 , 5 , 9 };
-	int B[] = { 0,0,0,0 };
 
-	F(A, B, 4);
-	   
+
+int main() {
+
+	char a[] = "DawnNews headlines, news stories, updates and latest news from Pakistan. Top political news, bulletins, talk shows, infotainment and much more.";
+
+	std::map<char, int> Counts;
+
+	for (int i = 0; i < strlen(a); i++)
+	{
+		if(!isSpecialChar(a[i]))
+		{
+			auto it = Counts.find(a[i]);
+			auto it_2 = Counts.begin();
+			if(std::isupper(a[i])) 
+				it_2 = Counts.find(char(a[i] + ('a' - 'A')));
+			else 
+				it_2 = Counts.find(char(a[i] - ('a' - 'A')));
+			
+			if (it == Counts.end() && it_2 == Counts.end())
+				Counts[a[i]]=0;
+		
+			Counts[a[i]]++;
+		}
+	}
+
+	for (auto i : Counts)
+	{
+		std::cout << i.first << ": " << i.second << std::endl;
+	}
 }
