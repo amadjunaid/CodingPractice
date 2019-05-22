0bbdab462e87cdffd5b21457332a967b7c61b9f7
AMD <amad.64bit@gmail.com>
2019-05-22 08:02:19 +0200

    1

--- General_Prep/InterviewTesting/Mutable.cpp
+++ General_Prep/InterviewTesting/Mutable.cpp
@@ -16,27 +16,69 @@ bool isSpecialChar(char r_c)
 }
 
 
+bool hasUniqueChars(char* a, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i+1; j < n; j++)
+			if (*(a + i) == *(a + j)) 
+				return false;
+	}
+
+	return true;
+}
+
+void reverse(char *str) {
+	char * end = str;
+	char tmp;
+	if (str) {
+		while (*end) {
+			++end;
+			
+		}
+		--end;
+		while (str < end) {
+			tmp = *str;
+			* str++ = *end;
+			* end-- = tmp;
+			}
+		}	
+}
+
+void removeDups(char *str) {
+	char * idx = str;
+
+	while (idx!='\0')
+	{
+		
+	}
+}
 
 int main() {
+	char a[] = "apple";
 
+	reverse(a);
+	std::cout << "Reversed array: " << a << std::endl;
+}
+
+void CountLetters()
+{
 	char a[] = "DawnNews headlines, news stories, updates and latest news from Pakistan. Top political news, bulletins, talk shows, infotainment and much more.";
 
 	std::map<char, int> Counts;
 
 	for (int i = 0; i < strlen(a); i++)
 	{
-		if(!isSpecialChar(a[i]))
+		if (!isSpecialChar(a[i]))
 		{
+			if (std::isupper(a[i]))
+				a[i] = char(a[i] + ('a' - 'A'));
 			auto it = Counts.find(a[i]);
-			auto it_2 = Counts.begin();
-			if(std::isupper(a[i])) 
-				it_2 = Counts.find(char(a[i] + ('a' - 'A')));
-			else 
-				it_2 = Counts.find(char(a[i] - ('a' - 'A')));
-			
-			if (it == Counts.end() && it_2 == Counts.end())
-				Counts[a[i]]=0;
-		
+
+
+			if (it == Counts.end())
+				Counts[a[i]] = 0;
+
 			Counts[a[i]]++;
 		}
 	}
